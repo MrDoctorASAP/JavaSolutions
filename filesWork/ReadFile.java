ee003545070ca723e2eed480a171771f6f877c1e
Bigsmoke rope gang <57768493+MrDoctorASAP@users.noreply.github.com>
2020-01-29 00:46:10 +0400

    Create ReadFile.java

---
+++ filesWork/ReadFile.java
@@ -0,0 +1,35 @@
+
+
+public static String[] ReadFile(String filePath) {
+    
+		ArrayList<String> list = new ArrayList<String>();
+		String[] content = null;
+		FileReader fr = null;
+		BufferedReader br = null;
+		
+    try {
+			
+      fr = new FileReader(filePath);
+			br = new BufferedReader(fr);
+			
+			String line = br.readLine();
+			
+      while(line != null) {
+				list.add(line);
+				line = br.readLine();
+			}
+			
+		}catch(Exception e) {
+			return null;
+		}
+		
+    content = new String[list.size()];
+		
+    for(int i = 0; i < content.length; i++) {
+			content[i] = list.get(i);
+		}
+		
+    return content;
+	}
+  
+  
